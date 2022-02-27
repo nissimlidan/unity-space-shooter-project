977af62efb93c1acb14f424fa12a770440093447
Lidan Nissim <nissimlidan@gmail.com>
2022-02-27 19:55:26 +0200

    laser
    add Laser

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,10 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     private float _speed = 3.5f;
+
+   [SerializeField]
+    private GameObject _laserPrefabs;
+   
     
     void Start()
     {
@@ -19,20 +24,27 @@ public class Player : MonoBehaviour
     {
         calculateMovement();
 
+        if(Input.GetKeyDown(KeyCode.Space)){
+            
+            Instantiate(_laserPrefabs, transform.position, UnityEngine.Quaternion.identity);
+        }
+
+        
+    
     }
 
     void calculateMovement()
     {
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
-        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        UnityEngine.Vector3 direction = new UnityEngine.Vector3(horizontalInput, verticalInput, 0);
         transform.Translate(direction * _speed * Time.deltaTime);
 
 
         //player position x is between -11.8f to 11.8f and y is between -3.8f to 0
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -11.8f, 11.8f), Mathf.Clamp(transform.position.y, -3.8f,5), 0);
+        transform.position = new UnityEngine.Vector3(Mathf.Clamp(transform.position.x, -11.8f, 11.8f), Mathf.Clamp(transform.position.y, -3.8f,5), 0);
 
     }
 }
